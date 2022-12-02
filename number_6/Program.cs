da50850abf4b71c6f7dabb0ec914c6f5adb7e14e
anna brezhneva <brezhneva.a@yandex.ru>
2022-12-02 15:45:48 +0300

    Решила задачу №6

---
+++ number_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2==0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
