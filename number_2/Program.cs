a11b953e98a23997b2f0f6a2390a17332f91842c
anna brezhneva <brezhneva.a@yandex.ru>
2022-12-02 14:12:59 +0300

    Код для вывода большего из двух чисел

---
+++ number_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} - max");
+}
+else{
+    Console.WriteLine($"{b} - max");
+}
